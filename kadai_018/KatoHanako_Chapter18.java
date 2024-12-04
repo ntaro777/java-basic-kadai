12faa505496fd9bcafb941fa995ce4f1c79a8850
ntaro777 <kurouji.natsuki@gmail.com>
2024-12-04 11:41:39 +0900

    kadai_018

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+
+    public KatoHanako_Chapter18() {
+        setGivenName("花子"); 
+    }
+
+    @Override
+    void eachIntroduce() {
+        System.out.println("私は設計が得意です");
+    }
+}
+
+
+//名前は加藤花子です
+//住所は東京都中野区〇×です
+//趣味は読書です
---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+
+    public KatoIchiro_Chapter18() {
+        setGivenName("一郎"); 
+    }
+
+    @Override
+    void eachIntroduce() {
+        System.out.println("私はPythonが得意です");
+    }
+}
+
+//名前は加藤一郎です
+//住所は東京都中野区〇×です
+//好きな食べ物はリンゴです
+
